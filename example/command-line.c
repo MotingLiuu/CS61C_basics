7ade69977f1ae1e2c24a197f1bb5d3757ee6403a
MotingLiuu <moting.liuu@gmail.com>
2025-05-31 10:18:34 +0900

    moved some material to another repo

--- example/command-line.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) { //argv[0] should be ./command-line or command-line
-        printf("%s ", argv[i]);
-    }
-    printf("\n");
-    return 0;
-}
